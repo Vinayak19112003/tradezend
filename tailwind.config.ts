bcf3d03b1bd73ca6dec323c7bd2c67c86a938527
Vinayak Deshmuk <vinayakdeshmuk191120033@gmail.com>
2025-08-13 16:53:40 +0000

    change the card componants design i want more attractive

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -91,10 +91,15 @@ export default {
             height: '0',
           },
         },
+        'glow': {
+            '0%': { 'background-position': '0% 50%' },
+            '100%': { 'background-position': '-200% 50%' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'glow': 'glow 5s linear infinite',
       },
     },
   },
